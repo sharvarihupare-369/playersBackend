09723e00f30717c6b7f77c8769e9a823f17401a1
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-08-07 17:45:03 +0530

    backend

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+require("dotenv").config()
+const cors = require("cors")
+server.use(cors())
+server.use(middlewares)
+server.use(router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log(`JSON Server is running on ${process.env.PORT}`)
+})
